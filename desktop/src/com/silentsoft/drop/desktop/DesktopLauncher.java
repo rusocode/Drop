e1f4af186b2f7e83a8714bf459f2c4cd52db23fc
Juan Debenedetti <juandebenedetti94@gmail.com>
2020-11-09 19:55:18 -0300

    Update files

---
+++ desktop/src/com/silentsoft/drop/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.silentsoft.drop.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.silentsoft.drop.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
